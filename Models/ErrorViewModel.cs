e6dc206d3132c40d834cf15cf62c7d62de9c9685
Arthur MUTIJIMA <amutijima@appolonnia.fr>
2024-03-07 09:51:36 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataGridGroupSummary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
